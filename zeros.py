254e2fb615c00459fed5f5c42763a6cda71fa297
Nigth-Prince <113068282+Nigth-Prince@users.noreply.github.com>
2023-06-25 12:34:49 +0300

    Обновление версии

--- zeros.py
+++ zeros.py
@@ -1,63 +1,65 @@
-import pygame as pg
+import time
 
-W = 600
-H = 600
-w = W // 7
-h = H // 7
-x = 0
-y = H // 2 - H // 2
-FPS = 60
+import pygame
 
+pygame.init()
+size_block = 200
+margin = 15
+width = height = size_block * 3 + margin * 4
+white = (255, 255, 255)
+mas = [[0] * 3 for i in range(3)]
+query = 0
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Крестики нолики')
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY = (125, 125, 125)
-LIGHT_BLUE = (64, 128, 255)
-GREEN = (0, 200, 64)
-YELLOW = (225, 255, 0)
-PINK = (230, 50, 230)
-BIR = (162, 80, 255)
+sound = pygame.mixer.Sound('win.mp3')
 
-rad = 50
+def rules():
+    pos = []
+    for a in range(3):
+        for b in range(3):
+            pos.append(mas[a][b])
+    for i in mas:
+        if i == ['x', 'x', 'x']:
+            sound.play()
+            time.sleep(1)
+            quit()
+    if pos[::3] == ['x', 'x', 'x'] or pos[::-3] == ['x', 'x', 'x'] or pos[1:10:3] == ['x', 'x', 'x']\
+            or pos[0:9:4] == ['x', 'x', 'x'] or pos[2:7:2] == ['x', 'x', 'x']:
+        sound.play()
+        time.sleep(1)
+        quit()
 
-values = {'x1': 130, 'x2': 190, 'x3': 250, 'x4': 310, 'x5': 370,
-          'y1': 180, 'y2': 240, 'y3': 300, 'y4': 360, 'y5': 420, 'dir4': 'right'}
-
-pg.init()
-win = pg.display.set_mode((W, H))
-pg.display.set_caption("Крестики - нолики")
-fps = pg.time.Clock()
-list = {1: (100,100), 2: (300,100), 3: (500,100), 4: (100,300), 5: (300,300),
-        6: (500,300), 7: (100,500), 8: (300,500), 9: (500,500)}
-left  = [(0,0), (200,100), (400,100)]
-right = [(200,100), (400,100)]
-win.fill(WHITE)
-f = 0
-l = 1
 while True:
 
-    for i in pg.event.get():
-        if i.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             exit()
-        if i.type == pg.MOUSEBUTTONDOWN:
-            for mouse in position:
-                f += 1
-                if i.pos > mouse and i.pos < position[f+1]:
 
-                    if i.button == 1: # отрисовка круга
-                        pg.draw.circle(win, (250,170,20), list[1], 80, 5, False)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
 
-                    elif i.button == 3: # kpectik
-                        pg.draw.line(win, (200, 100, 70), (i.pos[0] - 90,i.pos[1] - 90), (i.pos[0] + 90, i.pos[1] + 90))
-                        pg.draw.line(win, (200, 100, 70), (i.pos[0] - 90,i.pos[1] + 90), (i.pos[0] + 90, i.pos[1] - 90))
-                        print(i.pos)
-                        pg.display.update()
-                else: pass
-                l += 1
-    pg.draw.line(win, (0, 0, 0), (H // 3,0), (H // 3, W))
-    pg.draw.line(win, (0, 0, 0), (H // 3 * 2,0), (H // 3 * 2, W))
-    pg.draw.line(win, (0, 0, 0), (0, W // 3), (H, W // 3))
-    pg.draw.line(win, (0, 0, 0), (0, W // 3 * 2), (H, W // 3 * 2))
+            if mas[row][col] == 0:
+                if query % 2 == 0:
+                    mas[row][col] = 'x'
+                else:
+                    mas[row][col] = 'o'
+            query += 1
 
-    pg.display.update()
-    fps.tick(FPS)
+    for r in range(3):
+        for c in range(3):
+            x = c * size_block + (c + 1) * margin
+            y = r * size_block + (r + 1) * margin
+            pygame.draw.rect(win, white, (x, y, size_block, size_block))
+
+            if mas[r][c] == 'x':
+                pygame.draw.line(win,(155,70,220), (x + 5, y + 5), (x + size_block - 5, y + size_block - 5), 8)
+                pygame.draw.line(win, (70,220,155), (x + size_block - 5, y + 5), (x + 5, y + size_block - 5), 8)
+            elif mas[r][c] == 'o':
+                pygame.draw.circle(win, (220,155,70), (x + size_block // 2, y + size_block // 2),
+                                   size_block // 2 - 3, 8)
+
+    pygame.display.update()
+    rules()
